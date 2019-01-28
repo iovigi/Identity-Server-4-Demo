a8dc39482e195b01fa4670375c4ba87e3e839cde
Iliya Nedelchev <inedelchev@virtual-affairs.com>
2019-01-28 18:19:18 +0200

    add quickstarters

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 #This is demo of how Identity Server 4 should be setup. 
-Purpose of this project is to create base template of usage of Identity Server 4 with ASP.NET Coreл
+Purpose of this project is to create base template of usage of Identity Server 4 with ASP.NET Core.
+It is including also QuickStarteras from IdentityServer site
---
+++ src/quickstart/src/Client/Program.cs
@@ -0,0 +1,75 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // discover endpoints from metadata
+            var client = new HttpClient();
+            var disco = client.GetDiscoveryDocumentAsync("http://localhost:5000").Result;
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request token
+            var tokenResponse = client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            }).Result;
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            // request token
+            tokenResponse = client.RequestPasswordTokenAsync(new PasswordTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "ro.client",
+                ClientSecret = "secret",
+
+                UserName = "alice",
+                Password = "password",
+                Scope = "api1"
+            }).Result;
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            // call api
+            client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = client.GetAsync("http://localhost:5001/identity").Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
